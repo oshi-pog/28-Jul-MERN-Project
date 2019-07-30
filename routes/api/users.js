d0a7fa09d65fa1ed66494103b4e670db1227601d
josh-kea <josh0661@stud.kea.dk>
2019-07-31 01:51:13 +0200

    prettier fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty()
+  ],
+  (req, res) => {
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
 module.exports = router;
