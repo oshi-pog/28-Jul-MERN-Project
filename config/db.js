07f9ceabbfebc126119c81e44201be7f3a424d28
josh-kea <josh0661@stud.kea.dk>
2019-07-30 23:29:14 +0200

    DB connected small error

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+// Getting all the values from the config file we created earlier and imported, specifically the JSON object called mongoURI.
+const db = config.get('mongoURI');
+
+//Using async and await to make it look synchronous even though it is asyncronous
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+
+        console.log('MongoDB Connected...');
+    } catch(err) {
+        console.log(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
