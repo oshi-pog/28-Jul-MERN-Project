88b3cb979fb932277e47163855d739e162d3b4a9
josh-kea <josh0661@stud.kea.dk>
2019-07-31 01:58:23 +0200

    11. completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,10 +11,18 @@ router.post(
   [
     check('name', 'Name is required')
       .not()
-      .isEmpty()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
   ],
   (req, res) => {
-    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.send('User route');
   }
 );
