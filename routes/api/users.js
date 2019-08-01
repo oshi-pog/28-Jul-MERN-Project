3ed2200ffc6ac841336007655be4dbdf5d8b872f
josh-kea <josh0661@stud.kea.dk>
2019-08-01 15:10:15 +0200

    jsonwebtoken implemented fully, working user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -68,7 +70,22 @@ router.post(
       await user.save();
 
       //Return the json web token
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id // mongoose uses an abstraction so you dont have to use _id which is how it shows on mongoDB
+        }
+      };
+
+      //signing with webtoken, made inside of the config default
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
